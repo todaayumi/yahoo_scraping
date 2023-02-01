3ab8fe40bc1287281ecb532486b096cee135b796
todaayumi <ayumin.amg25@docomo.ne.jp>
2023-02-01 16:17:29 +0900

    DB保存仕分け

--- yahoo_scraping/app/Console/Commands/Scraping.php
+++ yahoo_scraping/app/Console/Commands/Scraping.php
@@ -41,51 +41,59 @@ class Scraping extends Command
     public function handle()
     {
         $goutte = GoutteFacade::request('GET', 'https://news.yahoo.co.jp/topics/top-picks');
-        $DB_lates = Article::orderBy('created_at', 'desc')->limit(8)->get();
+        $DB_lates = Article::orderBy('datetime', 'desc')->first();
 
         
         
-        $goutte->filter('.newsFeed_list')->each(function ($ul) {
-          $ul->filter('.newsFeed_item')->each(function ($li) {
-            $li->filter('.newsFeed_item_link')->each(function ($a){
-
-                //$article = new Article;
-                //$article->title = $a->filter('.newsFeed_item_title')->text();
-                //$article->url = $a->attr('href');
-                $datetime = $a->filter('time')->text();
-                //$article->save();
+        $goutte->filter('.newsFeed_list')->each(function ($ul) use ($DB_lates) {
+          $ul->filter('.newsFeed_item')->each(function ($li) use ($DB_lates){
+            $li->filter('.newsFeed_item_link')->each(function ($a) use ($DB_lates){
 
                 $year = date('Y');
                 $month = mb_strlen(date('n'));
                 $day = mb_strlen(date('j'));
+                $datetime = $a->filter('time')->text();
                 
                 if($month = 1){
                     if($day = 1){
-                        $date = $year .'-0' . str_replace('/', '-', substr($datetime, 0, 4))
+                        $date = $year .'-0' . substr($datetime, 0, 1) . '-0' . substr($datetime, 2, 1)
                                 . ' '. substr($datetime, 9, 13) . ':00';
                                 ;
                     }else{
-                        $date = $year . '-0' . str_replace('/', '-', substr($datetime, 0, 5))
+                        $date = $year . '-0' . str_replace('/', '-', substr($datetime, 0, 4))
                                 .' '. substr($datetime, 10, 14) . ':00';
                     }
                 }else{
-                    if($day = 1){
-                        $date = $year . '-0' . str_replace('/', '-', substr($datetime, 0, 5))
-                        .' '. substr($datetime, 10, 14) . ':00';
-                    }else{
-                        $date = $year . '-0' . str_replace('/', '-', substr($datetime, 0, 6))
-                        .' '. substr($datetime, 11, 15) . ':00';
-                    }
+                  if($day = 1){
+                      $date = $year . '-' .substr($datetime, 0, 2) . '-0' . substr($datetime, 3, 1)
+                      .' '. substr($datetime, 10, 14) . ':00';
+                  }else{
+                      $date = $year . '-' . str_replace('/', '-', substr($datetime, 0, 6))
+                      .' '. substr($datetime, 11, 15) . ':00';
+                  }
                 }
 
-                echo $date;
-            
-            
+                if(isset($DB_lates->datetime)){
+                    if($date > $DB_lates->datetime){
+                        
+                            $article = new Article;
+                            $article->title = $a->filter('.newsFeed_item_title')->text();
+                            $article->url =  $a->attr('href');
+                            $article->datetime =  $date;
+                            $article->save();
+                        }else{
+                            echo '最新です。';
+                            echo "\n";
+                        }
+                }else{
+                    $article = new Article;
+                     $article->title = $a->filter('.newsFeed_item_title')->text();
+                     $article->url =  $a->attr('href');
+                     $article->datetime =  $date;
+                     $article->save();
+                }
                 
-                
-                
-                
-            
+      
         });
                 
             });
